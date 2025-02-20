b74135b34a6baca68aabe10c25a6ab952399c2db
jamile <hellenbarbosam@gmail.com>
2025-02-20 02:51:41 -0300

    algumas alterações

--- database/seed_purchase_status.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO purchase_status (name, description) VALUES
-('Não Comprado', 'Item ainda não foi comprado'),
-('Comprado', 'Item já foi adquirido'),
-('Reservado', 'Item reservado para compra futura'),
-('Cancelado', 'Item cancelado da wishlist'); 
